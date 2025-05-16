dcaad58b7eaf55dbb1a049b424b85ce7ad9db6be
helven <helven.squall@gmail.com>
2025-05-16 14:11:41 +0800

    null value handling

--- src/components/FoodCard.tsx
+++ src/components/FoodCard.tsx
@@ -25,20 +25,20 @@ function FoodCard({ food }) {
         <div className="p-4">
           <div className="flex justify-between items-start">
             <Link href={`/foods/${food.id}`} className="food-name">{food.name}</Link>
-            <span className="food-calories">{Math.floor(food.calories_kcal)} kcal</span>
+            <span className="food-calories">{Math.floor(food.calories_kcal ?? 0)} kcal</span>
           </div>
           <div className="flex mt-4 space-x-4">
             <div className="food-macro">
               <span className="mr-1">🍖</span>
-              <span className="food-macro-text">{food.protein_g}g</span>
+              <span className="food-macro-text">{food.protein_g ?? 0}g</span>
             </div>
             <div className="food-macro">
               <span className="mr-1">🌾</span>
-              <span className="food-macro-text">{food.carbs_g}g</span>
+              <span className="food-macro-text">{food.carbs_g ?? 0}g</span>
             </div>
             <div className="food-macro">
               <span className="mr-1">🥑</span>
-              <span className="food-macro-text">{food.fat_g}g</span>
+              <span className="food-macro-text">{food.fat_g ?? 0}g</span>
             </div>
           </div>
           <div className="mt-3 text-left">
