69791da7b53eb9af19a941ce0217a015d29c4cc0
helven <helven.squall@gmail.com>
2025-05-17 10:35:20 +0800

    - CSS cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { useRoutes } from 'react-router-dom'
 // 2. Asset imports
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import '@/App.css'
-import '@/styles/global.css'
 
 // 3. Component imports
 import appRoutesConfig from '@/routes/AppRoutes'
